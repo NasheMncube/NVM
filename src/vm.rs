db47fc8d89de869a50e0f5703cf3e8e234b51a13
TuringDisciple <mncubenashe@gmail.com>
2018-09-29 16:57:52 +0100

    Added initializer function for VM@

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,5 @@
-pub enum INSTR_SET {
+#[derive(Debug, PartialEq)]
+pub enum InstrSet {
     PUSHi, // PUSH argument to stack
     PUSHA, // PUSH value in register A to stack
     PUSHB, // ...................... B to stack
@@ -26,22 +27,65 @@ pub enum INSTR_SET {
     HALT,  // HALT execution of VM
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Flag {
+    OVERFLOW,
+    ZERO,
+    NEGATIVE,
+    CARRY,
+    DEFAULT,
+}
+
 pub struct VM {
     A: u8,
     B: u8,
     X: u8,
     Y: u8, 
     SP: u8,
-    PC: u8,
-    CC: FLAG,
+    PC: *const InstrSet,
+    CC: Flag,
 
     mem: [u8; 256],
 }
 
-pub enum FLAG {
-    OVERFLOW
-    ZERO,
-    NEGATIVE,
-    CARRY,
+
+impl VM {
+    fn new(program: &[InstrSet]) -> VM {
+        VM {
+           A: 0,
+           B: 0,
+           X: 0,
+           Y: 0,
+           SP: 255,
+           PC: &program[0],
+           CC: Flag::DEFAULT,
+
+           mem: [0; 256],
+        }
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn initialize_new_VM() {
+        let program = [InstrSet::HALT];
+        let vm = VM::new(&program);
+        assert_eq!(vm.A, 0);
+        assert_eq!(vm.B, 0);
+        assert_eq!(vm.X, 0);
+        assert_eq!(vm.Y, 0);
+        assert_eq!(vm.SP, 255);
+        assert_eq!(vm.PC, &program[0]);
+        assert_eq!(vm.CC, Flag::DEFAULT);
+
+        let mut size = 0;
+        for x in vm.mem.iter() {
+            size += 1;
+            assert_eq!(*x, 0);
+        }
+        assert_eq!(size, 256);
+    }
+}
